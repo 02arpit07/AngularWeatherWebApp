8bfb280abc5250b89d0ef3d6c3d0dc6e6e679ae3
ARPIT VASHISHT <arpitvashisht1@gmail.com>
2020-03-28 01:43:52 +0530

    Added Searching Functionality

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -9,24 +9,29 @@ export class WeatherWidgetMainComponent implements OnInit {
 
   WeatherData: any;
   constructor() { }
+  city = '';
 
   ngOnInit() {
     this.WeatherData = {
       main : {},
       isDay: true
     };
-    this.getWeatherData();
+    this.getWeatherData(this.city);
     console.log(this.WeatherData);
   }
 
-  getWeatherData() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + ' toronto ' + ' &appid=5405cb0ef51ffce9359b796635f6c6e6')
+  getWeatherData(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5405cb0ef51ffce9359b796635f6c6e6`)
       .then(response => response.json())
       .then(data => {this.setWeatherData(data); });
 
     // tslint:disable-next-line:max-line-length
     // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
     // this.setWeatherData(data);
+  }
+   func1(city) {
+    this.city = city;
+    this.getWeatherData(city);
   }
 
   setWeatherData(data) {
